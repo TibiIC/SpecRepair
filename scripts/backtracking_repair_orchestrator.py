7d357e9bab2158120bda6113da16841dc1f51721
TibiIC <tibi.geo@ic.ac.uk>
2024-07-03 17:41:15 +0100

    FEAT: Guarantee Weakening of Specifications in BFS

--- scripts/backtracking_repair_orchestrator.py
+++ scripts/backtracking_repair_orchestrator.py
@@ -7,6 +7,7 @@ from spec_repair.components.counter_trace import CounterTrace
 from spec_repair.components.spec_learner import SpecLearner
 from spec_repair.components.spec_oracle import SpecOracle
 from spec_repair.enums import Learning
+from spec_repair.exceptions import NoWeakeningException
 from spec_repair.heuristics import first_choice
 from spec_repair.special_types import StopHeuristicType
 from spec_repair.wrappers.spec import Spec
@@ -17,13 +18,23 @@ class RepairNode:
             self,
             spec: list[str],
             ct_list: Optional[any],
-            learning_choice: str,
+            learning_hypothesis: str,
             learning_type: Learning
     ):
         self.spec = spec
         self.ct_list = ct_list
-        self.learning_hypothesis = learning_choice
+        self.learning_hypothesis = learning_hypothesis
         self.learning_type = learning_type
+        self.weak_spec_history = []
+
+    def __eq__(self, other):
+        return (self.spec == other.spec and
+                self.ct_list == other.ct_list and
+                self.learning_hypothesis == other.learning_hypothesis and
+                self.learning_type == other.learning_type)
+
+    def __hash__(self):
+        return hash((tuple(self.spec), tuple(self.ct_list), tuple(self.learning_hypothesis), self.learning_type))
 
 
 class BacktrackingRepairOrchestrator:
@@ -43,6 +54,7 @@ class BacktrackingRepairOrchestrator:
         stack: Deque[RepairNode] = deque()
         visited_nodes: Set[RepairNode] = set()
         unique_specs = SpecRecorder()
+        # TODO: what if the first spec cannot be weakened by assumption weakening?
         hypotheses = self._learner.find_weakening_hypotheses(spec, trace, [], Learning.ASSUMPTION_WEAKENING)
         for hypothesis in hypotheses:
             node = RepairNode(spec, [], hypothesis, Learning.ASSUMPTION_WEAKENING)
@@ -51,20 +63,44 @@ class BacktrackingRepairOrchestrator:
                 visited_nodes.add(node)
 
         while stack:
-            repair_node = stack.pop()
-            new_spec = self._learner.integrate_learning_hypothesis(repair_node.spec, repair_node.learning_hypothesis, repair_node.learning_type)
-            cs = self._oracle.synthesise_and_check(new_spec)
-            if not cs:
-                unique_specs.add(Spec(''.join(new_spec)))
-            else:
-                ct_list = copy(repair_node.ct_list)
-                ct_list.append(self.ct_from_cs(cs))
-                hypotheses = self._learner.find_weakening_hypotheses(spec, trace, ct_list, repair_node.learning_type)
-                for hypothesis in hypotheses:
-                    node = RepairNode(spec, ct_list, hypothesis, repair_node.learning_type)
-                    if node not in visited_nodes:
-                        stack.append(node)
-                        visited_nodes.add(node)
+            node = stack.pop()
+            try:
+                new_spec = self._learner.integrate_learning_hypothesis(node.spec, node.learning_hypothesis, node.learning_type)
+                cs = self._oracle.synthesise_and_check(new_spec)
+                if not cs:
+                    unique_specs.add(Spec(''.join(new_spec)))
+                else:
+                    ct_list = copy(node.ct_list)
+                    ct_list.append(self.ct_from_cs(cs))
+                    hypotheses = self._learner.find_weakening_hypotheses(spec, trace, ct_list, node.learning_type)
+                    for hypothesis in hypotheses:
+                        match node.learning_type:
+                            case Learning.ASSUMPTION_WEAKENING:
+                                node = RepairNode(copy(spec), copy(ct_list), hypothesis, node.learning_type)
+                            case Learning.GUARANTEE_WEAKENING:
+                                node = RepairNode(copy(new_spec), copy(ct_list), hypothesis, node.learning_type)
+                            case _:
+                                raise ValueError("Invalid learning type")
+                        node.weak_spec_history.append(new_spec)
+                        if node not in visited_nodes:
+                            stack.append(node)
+                            visited_nodes.add(node)
+            except NoWeakeningException as e:
+                print(str(e))
+                node = copy(node)
+                node.spec = node.weak_spec_history[0]
+                node.ct_list = node.ct_list[:1]
+                node.learning_hypothesis = None
+                node.learning_type = Learning.GUARANTEE_WEAKENING
+                if node not in visited_nodes:
+                    visited_nodes.add(node)
+                    hypotheses = self._learner.find_weakening_hypotheses(node.spec, trace, node.ct_list, node.learning_type)
+                    for hypothesis in hypotheses:
+                        node = copy(node)
+                        node.learning_hypothesis = hypothesis
+                        if node not in visited_nodes:
+                            stack.append(node)
+                            visited_nodes.add(node)
 
         return unique_specs
 
--- tests/test_scripts/test_backtracking_repair_orchestrator.py
+++ tests/test_scripts/test_backtracking_repair_orchestrator.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 from unittest import TestCase
 
 from scripts.backtracking_repair_orchestrator import BacktrackingRepairOrchestrator
@@ -43,7 +44,8 @@ class TestBacktrackingRepairOrchestrator(TestCase):
         # Getting expected repairs
         expected_specs_recorder: SpecRecorder = SpecRecorder()
         for spec_file in os.listdir('./test_files/lift_weakenings'):
-            expected_specs_recorder.add(Spec(''.join(format_spec(read_file_lines(f'./test_files/lift_weakenings/{spec_file}')))))
+            expected_specs_recorder.add(
+                Spec(''.join(format_spec(read_file_lines(f'./test_files/lift_weakenings/{spec_file}')))))
         expected_specs: list = expected_specs_recorder.get_specs()
         expected_specs.sort()
 
@@ -67,7 +69,59 @@ class TestBacktrackingRepairOrchestrator(TestCase):
         # Getting expected repairs
         expected_specs_recorder: SpecRecorder = SpecRecorder()
         for spec_file in os.listdir('./test_files/arbiter_weakenings'):
-            expected_specs_recorder.add(Spec(''.join(format_spec(read_file_lines(f'./test_files/arbiter_weakenings/{spec_file}')))))
+            expected_specs_recorder.add(
+                Spec(''.join(format_spec(read_file_lines(f'./test_files/arbiter_weakenings/{spec_file}')))))
+        expected_specs: list = expected_specs_recorder.get_specs()
+        expected_specs.sort()
+
+        self.assertEqual(expected_specs, new_specs)
+
+    def test_bfs_repair_spec_traffic_updated(self):
+        spec: list[str] = format_spec(read_file_lines(
+            './test_files/traffic/traffic_updated_strong.spectra'))
+        trace: list[str] = read_file_lines(
+            "./test_files/traffic/traffic_updated_auto_violation.txt")
+        repairer: BacktrackingRepairOrchestrator = BacktrackingRepairOrchestrator(SpecLearner(), SpecOracle())
+
+        # Getting all possible repairs
+        new_specs_recorder: SpecRecorder = repairer.repair_spec_bfs(spec, trace)
+        new_specs: list[str] = new_specs_recorder.get_specs()
+        new_specs.sort()
+        out_test_dir_name = "./test_files/out/traffic_test_bfs"
+        for i, new_spec in enumerate(new_specs):
+            write_to_file(f"{out_test_dir_name}/traffic_test_fix_{i}.spectra", new_spec)
+
+        # Getting expected repairs
+        expected_specs_recorder: SpecRecorder = SpecRecorder()
+        for spec_file in os.listdir('./test_files/traffic_weakenings'):
+            expected_specs_recorder.add(
+                Spec(''.join(format_spec(read_file_lines(f'./test_files/traffic_weakenings/{spec_file}')))))
+        expected_specs: list = expected_specs_recorder.get_specs()
+        expected_specs.sort()
+
+        self.assertEqual(expected_specs, new_specs)
+
+    @unittest.skip("Probably takes way too long to finalise")
+    def test_bfs_repair_spec_minepump(self):
+        spec: list[str] = format_spec(read_file_lines(
+            './test_files/minepump_strong.spectra'))
+        trace: list[str] = read_file_lines(
+            "./test_files/minepump_strong_auto_violation.txt")
+        repairer: BacktrackingRepairOrchestrator = BacktrackingRepairOrchestrator(SpecLearner(), SpecOracle())
+
+        # Getting all possible repairs
+        new_specs_recorder: SpecRecorder = repairer.repair_spec_bfs(spec, trace)
+        new_specs: list[str] = new_specs_recorder.get_specs()
+        new_specs.sort()
+        out_test_dir_name = "./test_files/out/minepump_test_bfs"
+        for i, new_spec in enumerate(new_specs):
+            write_to_file(f"{out_test_dir_name}/minepump_test_fix_{i}.spectra", new_spec)
+
+        # Getting expected repairs
+        expected_specs_recorder: SpecRecorder = SpecRecorder()
+        for spec_file in os.listdir('./test_files/minepump_weakenings'):
+            expected_specs_recorder.add(
+                Spec(''.join(format_spec(read_file_lines(f'./test_files/minepump_weakenings/{spec_file}')))))
         expected_specs: list = expected_specs_recorder.get_specs()
         expected_specs.sort()
 
